048c7ef8eeeb54f31a5ff892de3c46ae2117c6fc
Bas Kuis <b@ukora.com>
2020-12-24 11:49:07 -0600

    list of suspects

--- src/findsanta/SantaFinder.java
+++ src/findsanta/SantaFinder.java
@@ -3,8 +3,23 @@ package findsanta;
 public class SantaFinder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("hello world");
+		SantaSuspects santaSuspects = new SantaSuspects(10);
+		santaSuspects.addSuspect("Bob Bezanga");
+		santaSuspects.addSuspect("Sally Zimmers");
+		santaSuspects.addSuspect("Peter Pinkelton");
+		santaSuspects.addSuspect("Bobby Drop Tables");
+		santaSuspects.addSuspect("Rudolph Rednose");
+		santaSuspects.addSuspect("Bob Ross");
+		santaSuspects.addSuspect("Ubiga Altman");
+		santaSuspects.addSuspect("Tony Ross");
+		santaSuspects.addSuspect("Aaron Rogers");
+		santaSuspects.addSuspect("Yolo Once");
+		santaSuspects.addSuspect("Yoka Yamalla");
+		santaSuspects.addSuspect("Big Show");
+		santaSuspects.addSuspect("Doug Dimmadome");
+
+		santaSuspects.printSuspects();
+		
 	}
 
 }
